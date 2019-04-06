8488873d0bd8f7bac2d4f33ceeb37b422297a1d0
huangxz <huangxz@meicloud.com>
2019-04-06 20:29:13 +0800

    initialize the menu

---
+++ main.py
@@ -0,0 +1,13 @@
+choices = {
+	1 : 'View the address book',
+	2 : 'Add a contact',
+	3 : 'Modify a contact',
+	4 : 'delete a contact',
+	5 : 'search in address book'
+}
+print('Welcome! Choose a number to use this program!')
+for choice, desc in choices.items():
+	print(choice,'.',desc)
+
+
+
